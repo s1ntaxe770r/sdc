2cb209b660a2812fe6e69e7c18bfb1a7f3416cb2
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-14 15:37:23 +0100

    finish upload route

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask import Flask,request
 from flask import json
 from flask.json import jsonify
 from flask_sqlalchemy import SQLAlchemy
-import datetime
 from sqlalchemy.orm import backref
-from flask_marshmallow import Marshmallow , fields
-from utils import Init_Uploads,allowed_file
+from utils import Init_Uploads,allowed_file,Random_File
 from passlib.hash import bcrypt 
-from tasks import upload_file
-from redis import Redis
-from rq import Queue
+import pathlib
+from flask.globals import request
+from werkzeug.utils import secure_filename
+import os
+import requests
 
 
 
@@ -19,19 +19,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = 'uploads'
 ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg','webp'])
-redis_conn= Redis()
-q = Queue(connection=redis_conn)
+
+
 
 
 Init_Uploads()
 
-
-
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
-db.drop_all()
-db.create_all()
+
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -76,7 +74,7 @@ def create_user():
             'account_created': 'true',
         }
     )
-    
+ 
 @app.route('/upload',methods=['POST'])
 def upload():
     if 'files[]' not in request.files:
@@ -87,19 +85,23 @@ def upload():
     print('none here either')
     for file in files:
         if file and allowed_file(file.filename,ALLOWED_EXTENSIONS):
-            print('files get qued')
-            q.enqueue(upload_file,file)
-            success_msg = 'your files would be uploaded shortly'
-            print('json failing?')
+            uploaded_files = 0 
+            secure_file , filext = os.path.splitext(secure_filename(file.filename))
+            Random_Filename = Random_File.generate()
+            newfilename = Random_Filename + filext
+            
+            if file.save(os.path.join(app.config['UPLOAD_FOLDER'], newfilename)):
+                uploaded_files +=1
+                os.rename(secure_file,newfilename)
+            success_msg = 'uploaded file(s)'
             return jsonify({'uploaded':"True",'message':success_msg})
-        print('prob return val')
-        return jsonify({'err':'sorry one of the files is not allowed'})  
+        
+        return jsonify({'err':'sorry one of the files is not allowed'})
                 
      
 
     
     
-
 
 if __name__ == "__main__":
     app.run(debug=True)
