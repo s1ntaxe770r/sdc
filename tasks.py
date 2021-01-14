c09ac7b8f1102371af80cec7ec12d0b503ab450d
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-14 15:39:06 +0100

    remove celery

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.utils import secure_filename
-import os
-
-
-
-def upload_file(file ,app):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
