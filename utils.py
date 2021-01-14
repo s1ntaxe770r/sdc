c5774556825fcc28a9bc8363ee68a7fa115ea564
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-14 15:38:38 +0100

    add util for rand file names

--- utils.py
+++ utils.py
@@ -14,7 +14,13 @@ def Init_Uploads():
     if not os.path.isdir(UPLOAD_FOLDER):
         os.mkdir(UPLOAD_FOLDER)
 
-
-
+class Random_File:
+ 	@classmethod
+ 	def generate(cls,size=6):
+ 		characters = ascii_letters  +  digits + ascii_lowercase
+ 		choice = random.SystemRandom()
+ 		password =  "".join(choice.choice(characters)
+                    for i in range(size))
+ 		return str(password)
 def allowed_file(filename , ALLOWED_EXTENSIONS):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
