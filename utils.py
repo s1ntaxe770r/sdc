da321d42b51dea8fd34c8af14f782326adc12def
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-14 06:42:54 +0100

    add util to generate filenames

--- utils.py
+++ utils.py
@@ -1,8 +1,24 @@
 from passlib.hash import bcrypt 
-
+import os 
+import random
+from string import ascii_letters,ascii_lowercase,digits
 
 def hash_password(password:str):
     hashed = bcrypt.hash(password)
     return str(hash) 
 
-print(hash_password('hi'))
+
+def Init_Uploads():
+    path = os.getcwd()
+    UPLOAD_FOLDER = os.path.join(path, 'uploads')
+    if not os.path.isdir(UPLOAD_FOLDER):
+        os.mkdir(UPLOAD_FOLDER)
+
+class Password:
+ 	@classmethod
+ 	def generate(cls,size=16):
+ 		characters = ascii_letters  +  digits + ascii_lowercase
+ 		choice = random.SystemRandom()
+ 		password =  "".join(choice.choice(characters)
+                    for i in range(size))
+ 		return str(password)
